eba3c77e1d56ee8de401be7a46604d5c1a24e416
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-03-04 16:28:41 +0100

    cambios

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
+// import AppDos from './AppDos.tsx'
 import AppTres from './AppTres.tsx'
+// import AppCuatro from './AppCuatro.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App/> */}
     {/* <AppDos /> */}
-    <AppTres/>
+    <AppTres />
+    {/* <AppCuatro/> */}
   </StrictMode>,
 )
