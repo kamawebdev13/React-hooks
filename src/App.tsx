069aab1e27a421a5a09ae0db560e2c20084e9bd7
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-03-04 21:54:55 +0100

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import StockBadge from './componentes/StockBadge'
-import PlanCard from './componentes/PlanCard'
-import './componentes/index.css'
+import ContadorSkills from './componentes/ContadorSkills';
+import './components/index.css'
 
 function App() {
+  
   return (
-    <div className="min-h-screen bg-slate-50 rounded-lg p-8">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        Galería de Productos y Planes
+    <div className="min-h-screen  bg-slate-100 p-8">
+      <h1 className="text-4xl font-extrabold text-center text-slate-900 mb-12">
+       ✨ Contador de Habilidades ✨
       </h1>
-      
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {/* Sección de Stock */}
-        <StockBadge productName="Iphone 17" quantity={5} />
-        <StockBadge productName="Samsung galaxy" quantity={50} />
-        <StockBadge productName="Huawei p9" quantity={0} />
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         
-        {/* Sección de Planes */}
-        <PlanCard nombrePlan="Básico" precio={20} esPopular={false} />
-        <PlanCard nombrePlan="Premium" precio={50} esPopular={true} />
+        <ContadorSkills/>
+        
+        <ContadorSkills/>
+       
+        
       </div>
     </div>
-  )
-}
+
+    
+  );
+};
 
 export default App
